10eb24cf7f840d143abee62290b463fefc3d6922
yulun72 <61811182+yulun72@users.noreply.github.com>
2020-04-16 01:20:11 +0800

    week6 homework 1

---
+++ homework.py
@@ -0,0 +1,12 @@
+# 1.用input()函數取得姓名、年紀與體重的資訊
+## 用變數儲存資料
+# 2.用print()函數螢幕列印特定內容
+
+name = input("請問貴姓大名？")
+print("你好，" , name)
+
+y = int(input('請問年紀？'))
+print('原來你', y,'歲')
+
+w = float(input('請問體重？'))
+print('體重為', w)
